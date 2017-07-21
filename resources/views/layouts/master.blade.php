5887f6c0e92a42722c50d851b54b07e5ac62ba34
Mbuyu Makayi <mbmakayi@gmail.com>
2017-07-21 13:08:35 +0100

    first commit

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>@yield('title')</title>
+</head>
+<body>
+  <div class="container">
+   <div>
+     @yield('content')
+   	
+   </div>
+  </div>
+
+</body>
+</html>
